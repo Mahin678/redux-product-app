fc9a071238bb401285f8fd89db5c16f9ad9183c6
Mahin678 <mahintazuar678@gmail.com>
2021-06-30 22:28:07 +0600

    code update

--- src/Redux/Action/ProductAction.js
+++ src/Redux/Action/ProductAction.js
@@ -1,12 +1,20 @@
 import * as Type from "./Type";
 const axios = require('axios');
-export const postView = () => async dispatch => {
-  const post = await axios.get('https://jsonplaceholder.typicode.com/posts');
-   dispatch({type: Type.VIEW_POST, payload:post});
-   console.log(post);
- return post;
-} 
 
+// export const postView = () => async dispatch => {
+//   const post = await axios.get('https://jsonplaceholder.typicode.com/posts');
+//    dispatch({type: Type.VIEW_POST, payload:post});
+//    console.log(post);
+//  return post;
+// } 
+export const postView = () => async (dispatch) => {
+  try {
+    const post = await axios.get('https://jsonplaceholder.typicode.com/posts');
+   dispatch({type: Type.VIEW_POST, payload:post});
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 export const productCreate = (data) => (dispatch) => {
   dispatch({ type: Type.PRODUCT_CREATE, payload: data });
 };
--- src/Test/Test.jsx
+++ src/Test/Test.jsx
@@ -3,20 +3,20 @@ import {useDispatch, useSelector} from 'react-redux';
 import {postView} from '../Redux/Action/ProductAction';
 
 const Test = () => {
-  const ch =  postView();
+    // post action active  
     const dispatch = useDispatch();
-
         useEffect(() => {
-         dispatch((postView()))
-        },[])
-
+         dispatch(postView())
+        },[dispatch])
 
   const MyComponent = () => {
+    //   for get data from selector
           const selector = useSelector(state => state.PostReducer);
           return selector.data;
         }
        const data =  MyComponent()
-    return (
+
+       return (
         <div>
             Test
         {data && data.map((info , i)=><div key={i} className="w-50 " > <h2 className="text-danger">{info.title}</h2> <h6   >{info.body}</h6>  </div>)}
